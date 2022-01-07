9b63ab4f8a5b11c017164e0a97196953ab70e1be
Yanis Dib <ynsprive@gmail.com>
2022-01-07 23:06:01 +0100

    Add AppRouter

---
+++ src/routers/AppRouter.jsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from 'react-router';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import App from '../App';
+import { Pokedex } from '../pages';
+
+
+function AppRouter() {
+    return (
+        <Router>
+            <main>
+                <Routes>
+                    <Route path='/' element={<App />} exact />
+                    <Route path='/pokedex' element={<Pokedex />} />
+                </Routes>
+            </main>
+        </Router>
+    )
+}
+
+
+export default AppRouter;
