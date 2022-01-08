715c481e2e6444ae905d2869281272014f2f8b7f
Yanis Dib <ynsprive@gmail.com>
2022-01-08 22:45:17 +0100

    Add index for components

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Container } from './Container/Container';
+
+export { default as SideBar } from './SideBar/SideBar';
+
+export { default as SearchBar } from './SearchBar/SearchBar';
