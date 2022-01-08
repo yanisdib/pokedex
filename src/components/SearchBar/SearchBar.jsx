d82699ea4362877a8c9a7cea2b623ab858caf1f0
Yanis Dib <ynsprive@gmail.com>
2022-01-08 22:41:20 +0100

    Add SearchBar component

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+
+const SearchBar = styled.input`
+    width: 90%;
+    min-height: 40px;
+    height: 45px;
+    background-color: #ffffff;
+    border-radius: 50px;
+`;
+
+
+export default SearchBar;
