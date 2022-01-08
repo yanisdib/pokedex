0e5433446f64833ead6fca3352f63f8c2f1db326
Yanis Dib <ynsprive@gmail.com>
2022-01-08 22:40:37 +0100

    Create Container component

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+
+export default styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    column-gap: 30px;
+    align-items: center;
+    max-width: 1450px;
+    min-height: 100vh;
+    margin: auto;
+`;
