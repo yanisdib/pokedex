ea16d09bb6741c91504299df8e7445f34ee9ba01
Yanis Dib <ynsprive@gmail.com>
2022-01-08 22:43:40 +0100

    Update actions for types

--- src/services/redux/actions/types.js
+++ src/services/redux/actions/types.js
@@ -1,3 +1,3 @@
-export const fetchTypesRequest = () => ({ type: 'pokemon/fetchTypesRequested' })
-export const fetchTypesSuccess = (types) => ({ type: 'pokemon/fetchTypesSucceeded', types })
-export const fetchTypesFailure = (error) => ({ type: 'pokemon/fetchTypesFailed', error })
+export const fetchTypesRequest = () => ({ type: 'types/fetchTypesRequested' })
+export const fetchTypesSuccess = (types) => ({ type: 'types/fetchTypesSucceeded', types })
+export const fetchTypesFailure = (error) => ({ type: 'types/fetchTypesFailed', error })
