03571661eab8b3d0a5f24572f445eadc0aff0eb5
Yanis Dib <ynsprive@gmail.com>
2022-01-09 21:46:22 +0100

    Add PokemonGrid component

---
+++ src/components/PokemonGrid/PokemonGrid.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+
+export default styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    column-gap: 20px;
+    row-gap: 30px;
+    min-width: 800px;
+    align-content: center;
+    justify-items: center;
+    margin: 30px 0;
+`;
