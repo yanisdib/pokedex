a2bb2114d871cb4fb00147919470b28ccfde02e7
Yanis Dib <ynsprive@gmail.com>
2022-01-09 21:44:38 +0100

    Add ThemeProvider to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyles from './assets/styles/js/global';
+import { lightTheme } from './assets/styles/js/theme';
+
 import { Pokedex } from './pages';
 
 
 function App() {
-  return <Pokedex></Pokedex>;
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <>
+        <GlobalStyles />
+        <Pokedex />
+      </>
+    </ThemeProvider>
+  );
 }
 
 
