1bcb32bcf7f684238e502927dc9d5475cffbef57
Yanis Dib <ynsprive@gmail.com>
2022-01-09 21:47:40 +0100

    Update tests for SideBar component

--- src/components/SideBar/test/SideBar.test.jsx
+++ src/components/SideBar/test/SideBar.test.jsx
@@ -1,10 +1,18 @@
 import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import usePokemonTypes from '../../../hooks/usePokemonTypes';
+
+import configureStore from '../../../services/redux/store/configureStore';
+
 import SideBar from '../SideBar';
+
 
 describe('SideBar component', () => {
     let wrapper;
+    const store = configureStore();
+    const ReduxProvider = ({ store, children }) => <Provider store={store}>{children}</Provider>;
 
-    beforeEach(() => wrapper = shallow(<SideBar />))
+    beforeAll(() => wrapper = shallow(<ReduxProvider store={store}><SideBar /></ReduxProvider>))
 
     it('renders properly without crashing', () => expect(wrapper).toMatchSnapshot())
 })
