ff61c3812111c47131da60b5b7021e641fcf9c5d
Yanis Dib <ynsprive@gmail.com>
2022-01-09 21:45:20 +0100

    Add new components

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,11 @@
+export { default as Tag } from './Tag/Tag';
+export { default as Spinner } from './Spinner/Spinner';
+
 export { default as Container } from './Container/Container';
 
 export { default as SideBar } from './SideBar/SideBar';
 
 export { default as SearchBar } from './SearchBar/SearchBar';
+
+export { default as PokemonGrid } from './PokemonGrid/PokemonGrid';
+export { default as PokemonCardPreview } from './PokemonCardPreview/PokemonCardPreview';
