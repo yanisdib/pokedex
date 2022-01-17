148f66eaab8ed0a40e988edadfbf796b7b1512a5
Yanis Dib <ynsprive@gmail.com>
2022-01-17 22:39:40 +0100

    Update styles

--- src/components/Tag/Tag.jsx
+++ src/components/Tag/Tag.jsx
@@ -5,6 +5,7 @@ export default styled.div`
     display: flex;
     align-content: center;
     width: fit-content;
+    height: fit-content;
     background-color: ${({ background }) => background};
     font-size: 0.85em;
     font-weight: 600;
@@ -15,5 +16,6 @@ export default styled.div`
     padding: 7px 15px;
     border-radius: 20px;
     cursor: pointer;
+    webkit-box-shadow: 0px 4px 14px 0px rgb(110 110 110 / 38%);
     box-shadow: 0px 4px 14px 0px rgb(110 110 110 / 38%);
 `;
