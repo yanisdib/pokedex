7e0114803021a04c4786ac67f1e5abfa4fb2b094
Yanis Dib <ynsprive@gmail.com>
2022-01-17 22:36:30 +0100

    Update components index

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,17 @@
 export { default as Tag } from './Tag/Tag';
 export { default as Spinner } from './Spinner/Spinner';
 export { default as LoadMoreButton } from './LoadMoreButton/LoadMoreButton';
+export { default as ProgressBar } from './ProgressBar/ProgressBar';
 
 export { default as Container } from './Container/Container';
+export { default as Modal } from './Modal/Modal';
 
 export { default as SideBar } from './SideBar/SideBar';
 
 export { default as SearchBar } from './SearchBar/SearchBar';
 
 export { default as PokemonGrid } from './PokemonGrid/PokemonGrid';
+
 export { default as PokemonCardPreview } from './PokemonCardPreview/PokemonCardPreview';
+export { default as PokemonCard } from './PokemonCard/PokemonCard';
+export { default as PokemonCardInformation } from './PokemonCardInformation/PokemonCardInformation';
