1f18b619220def88e3029d0e6d48bfcc76af8ce1
Yanis Dib <ynsprive@gmail.com>
2022-01-17 22:38:23 +0100

    Update PokemonCardPreview component

--- src/components/PokemonCardPreview/PokemonCardPreview.jsx
+++ src/components/PokemonCardPreview/PokemonCardPreview.jsx
@@ -9,33 +9,134 @@ import { Spinner, Tag } from '..';
 
 
 const CardPreview = styled.div`
-    display: flex;
-    flex-direction: column;
-    flex-wrap: wrap;
-    width: 15vw;
-    height: 30vh;
-    background: #ffffff;
-    border-radius: 20px;
-    border: 1px solid #00000026;
-    align-items: center;
-    webkit-box-shadow: 0px 4px 14px 0px rgb(110 110 110 / 38%);
-    box-shadow: 0px 4px 14px 0px rgb(110 110 110 / 38%);
+    /* Landscape phones and down */
+    @media (min-width: 320px) { 
+        display: flex;
+        flex-direction: column;
+        flex-wrap: wrap;
+        width: 45vw;
+        height: 40vh;
+        background: #ffffff;
+        border-radius: 20px;
+        border: 1px solid #00000026;
+        align-items: center;
+        justify-content: center;
+        padding: 30px 0;
+        webkit-box-shadow: 0px 4px 14px 0px rgb(110 110 110 / 38%);
+        box-shadow: 0px 4px 14px 0px rgb(110 110 110 / 38%);
+    }
+
+    /* Landscape phone to portrait tablet */
+    @media (max-width: 480px) { 
+        display: flex;
+        flex-direction: column;
+        flex-wrap: wrap;
+        width: 45vw;
+        height: 40vh;
+        background: #ffffff;
+        border-radius: 20px;
+        border: 1px solid #00000026;
+        align-items: center;
+        justify-content: center;
+        padding: 30px 0;
+        webkit-box-shadow: 0px 4px 14px 0px rgb(110 110 110 / 38%);
+        box-shadow: 0px 4px 14px 0px rgb(110 110 110 / 38%);
+    }
+
+    /* Portrait tablet to landscape and desktop */
+    @media (min-width: 768px) { 
+        display: flex;
+        flex-direction: column;
+        flex-wrap: wrap;
+        width: 28vw;
+        height: 30vh;
+        background: #ffffff;
+        border-radius: 20px;
+        border: 1px solid #00000026;
+        align-items: center;
+        justify-content: center;
+        padding: 30px 0;
+        webkit-box-shadow: 0px 4px 14px 0px rgb(110 110 110 / 38%);
+        box-shadow: 0px 4px 14px 0px rgb(110 110 110 / 38%);
+    }
+
+    /* Large desktop */
+    @media (min-width: 1200px) { 
+        display: flex;
+        flex-direction: column;
+        flex-wrap: nowrap;
+        width: 15vw;
+        height: 30vh;
+        background: #ffffff;
+        border-radius: 20px;
+        border: 1px solid #00000026;
+        align-items: center;
+        padding: 30px 0;
+        webkit-box-shadow: 0px 4px 14px 0px rgb(110 110 110 / 38%);
+        box-shadow: 0px 4px 14px 0px rgb(110 110 110 / 38%);
+    }
 `;
 
 const Thumbnail = styled.div`
-    display: flex;
-    position: relative;
-    flex-direction: row;
-    width: 100%;
-    justify-content: center;
-    align-items: center;
-    font-size: 8rem;
-    font-weight: 600;
-    color: #ff9ebe6b;
-
     img {
         position: absolute;
         margin: 0 auto;
+    }
+
+    /* Landscape phones and down */
+    @media (min-width: 320px) { 
+        display: flex;
+        position: relative;
+        flex-direction: row;
+        width: 100%;
+        justify-content: center;
+        align-items: center;
+        font-size: 4.5rem;
+        font-weight: 600;
+        color: #ff9ebe6b;
+        cursor: pointer;
+    }
+
+    /* Landscape phone to portrait tablet */
+    @media (max-width: 480px) { 
+        display: flex;
+        position: relative;
+        flex-direction: row;
+        width: 100%;
+        justify-content: center;
+        align-items: center;
+        font-size: 4.5rem;
+        font-weight: 600;
+        color: #ff9ebe6b;
+        cursor: pointer;
+    }
+
+    /* Portrait tablet to landscape and desktop */
+    @media (min-width: 768px) { 
+        display: flex;
+        position: relative;
+        flex-direction: row;
+        width: 100%;
+        justify-content: center;
+        align-items: center;
+        font-size: 8rem;
+        font-weight: 600;
+        color: #ff9ebe6b;
+        cursor: pointer;
+    }
+
+    /* Large desktop */
+    @media (min-width: 1200px) { 
+        display: flex;
+        position: relative;
+        flex-direction: row;
+        width: 100%;
+        justify-content: center;
+        align-items: center;
+        font-size: 8rem;
+        font-weight: 600;
+        color: #ff9ebe6b;
+        cursor: pointer;
     }
 `;
 
@@ -60,14 +161,20 @@ const TypeTags = styled.div`
 `;
 
 
-function PokemonCardPreview({ name, number, sprite, types }) {
+function PokemonCardPreview({
+    name,
+    number,
+    sprite,
+    types,
+    onThumbnailClick
+}) {
 
     const typesList = types.map(current => current.type.name);
 
     return (
         <Suspense fallback={<Spinner />}>
             <CardPreview>
-                <Thumbnail>
+                <Thumbnail data-pokemon={name} onClick={onThumbnailClick}>
                     <p>{number}</p>
                     <img
                         src={sprite}
@@ -75,7 +182,7 @@ function PokemonCardPreview({ name, number, sprite, types }) {
                         title={name}
                     />
                 </Thumbnail>
-                <Name>{name}</Name>
+                <Name >{name}</Name>
                 <TypeTags>
                     {
                         typesList.map(type => {
