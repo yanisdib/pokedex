2bcec6d34378fef6516e7063c5a1cb4555b3b890
Yanis Dib <ynsprive@gmail.com>
2022-01-17 22:36:15 +0100

    Update Container.jsx

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -2,6 +2,47 @@ import styled from 'styled-components';
 
 
 export default styled.div`
+/* Landscape phones and down */
+@media (min-width: 320px) {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    column-gap: 30px;
+    align-items: center;
+    justify-content: center;
+    max-width: 1450px;
+    min-height: 100vh;
+    margin: auto;
+}
+
+/* Landscape phone to portrait tablet */
+@media (min-width: 480px) {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    column-gap: 30px;
+    align-items: center;
+    justify-content: center;
+    max-width: 1450px;
+    min-height: 100vh;
+    margin: auto;
+}
+
+/* Portrait tablet to landscape and desktop */
+@media (min-width: 768px) {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    column-gap: 30px;
+    align-items: center;
+    justify-content: center;
+    max-width: 1450px;
+    min-height: 100vh;
+    margin: auto;
+}
+
+/* Large desktop */
+@media (min-width: 1200px) {
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
@@ -11,4 +52,5 @@ export default styled.div`
     max-width: 1450px;
     min-height: 100vh;
     margin: auto;
+}
 `;
