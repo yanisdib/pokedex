d10a4fd1f92fb511cc817cb1db49eda04a5993fb
Yanis Dib <ynsprive@gmail.com>
2022-01-17 22:40:00 +0100

    Create index for hooks

---
+++ src/hooks/index.js
@@ -0,0 +1,4 @@
+export { default as usePokemon } from './usePokemon';
+export { default as usePokemonTypes } from './usePokemonTypes';
+export { default as useFormField } from './useFormField';
+export { default as useModal } from './useModal';
